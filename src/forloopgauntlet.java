3cf92216c3858355eba7fa2e1a611428d24e5069
chambersl <league@iMac-2.attlocal.net>
2018-10-16 18:27:45 -0700

    forloopgauntel

---
+++ src/forloopgauntlet.java
@@ -0,0 +1,77 @@
+
+public class forloopgauntlet {
+	public static void main(String[] args) {
+
+		/*
+		 * for (int i = 0; i <101; i++) {
+		 * 
+		 * System.out.println(""+i+"");
+		 * 
+		 * 
+		 * }
+		 *
+		 * 
+		 * for (int i = 100; i >0; i--) { System.out.println(""+i+""); }
+		 * 
+		 * 
+		 * 
+		 * for (int i = 0; i < 100; i++) {
+		 * 
+		 * if (i%2==0) { System.out.println(i+" is even!"); }
+		 * 
+		 * }
+		 * 
+		 * for (int i = 0; i < 100; i++) {
+		 * 
+		 * if (i%2==1) { System.out.println(i+" is odd!"); }
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 
+		 * for (int i = 0; i < 500; i++) {
+		 * 
+		 * if (i%2==1) { System.out.println(i+" is odd!"); }
+		 * 
+		 * if (i%2==0) { System.out.println(i+" is even!"); } }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * for (int i = 0; i < 784; i+=7) {
+		 * 
+		 * System.out.println(i);
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 
+		 * for (int i = 2007; i <2019 ; i++) {
+		 * System.out.println("In "+i+" I was "+(i-2007) +" years old"); }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * for (int i = 0; i < 3; i++) { for (int j = 0; j < 3; j++) {
+		 * System.out.println(i+""+j); }
+		 * 
+		 * }
+		 */
+		for (int i = 0; i < 7; i+=3) {
+			
+		for (int j = 1; j < 4; i++) {
+		System.out.print(j);
+		}
+		System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
