17b1b6371b9d5cd2ae61a639f7655194f4b652c8
rtgamer8 <40346026+rtgamer8@users.noreply.github.com>
2018-11-06 18:34:18 -0800

    circle ring, and guessing pie

---
+++ CircleRing.java
@@ -0,0 +1,51 @@
+package nested_loops;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Level 0
+ */
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class CircleRing {
+	public static void main(String[] args) throws Exception {
+
+		// 1. Make a new Robot
+		Robot ron = new Robot();
+ron.miniaturize();
+		// 2. Set your robot’s position to x=150 and y=200
+		ron.setX(150);
+		ron.setY(200);
+		// 3. Put the robot's pen down
+		ron.penDown();
+		// 4. Set the robot’s speed to 10
+		ron.setSpeed(200);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+
+			// 6. Move the robot 3 pixels
+			ron.setPenWidth(10);
+			ron.setRandomPenColor();
+			ron.move(3);
+			// 7. Turn the robot 1 degree
+			ron.turn(1);
+			// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
+			if (i % 20 == 0) {
+				// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
+				for (int j = 0; j < 360; j++) {
+
+					// 10. Move the robot 1 pixel
+					ron.setPenWidth(10);
+					ron.setRandomPenColor();
+					ron.move(1);
+					
+					// 11. Turn the robot 1 degree
+					ron.turn(1);
+					
+				}
+			}
+		
+			
+		}
+		ron.hide();
+	}
+}
